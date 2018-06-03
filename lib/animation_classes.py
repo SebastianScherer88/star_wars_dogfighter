3fcd72ebb33b5843e787946670d0b5e50412c4b1
Sebastian Scherer <Sebastian Scherer>
2018-06-03 23:58:08 +0100

    deleted old animation class

--- lib/animation_classes.py
+++ lib/animation_classes.py
@@ -14,13 +14,9 @@ Created on Sat Jun  2 14:00:12 2018
  sprite through time (like gun flashes, missile propulsion, engine fire etc.)'''
  
 #from pygame.sprite import Sprite, Group
-from sprite_classes import MaskedSprite, BasicSprite
-from pygame.sprite import Group
+from sprite_classes import BasicSprite
  
-import pygame as pg
 import numpy as np
-
-import sys
 
 class BasicAnimationNew(BasicSprite):
     '''Base class for animations used in game.'''
@@ -116,110 +112,3 @@ class BasicAnimationNew(BasicSprite):
             self._image_index = image_number - 1
             # call base class update
             BasicSprite.update(self)
-
-class BasicAnimation(MaskedSprite):
-    
-    def __init__(self,
-                 screen,
-                 animation_meta_data,
-                 animation_sound_object,
-                 frames_per_second,
-                 *groups,
-                 angle=0,
-                 speed=0,
-                 center=np.array([0,0]).astype('float')):
-        
-        MaskedSprite.__init__(self,
-                             screen,
-                             animation_meta_data,
-                             *groups,
-                             angle=angle,
-                             speed=speed,
-                             center=center)
-        
-        # load sound file specified in metadat and play
-        #animation_sound = pg.mixer.Sound(file=animation_meta_data['sound_path'])
-        #animation_sound.play() # play on any channel available
-        
-        animation_sound_object.play()
-        
-        # set last image index
-        self.last_image_index = len(self._original_images) - 1
-        
-        # get frames per image ratio
-        self.frames_per_image = frames_per_second * animation_meta_data['seconds_per_image']
-        
-        # start counter
-        self.counter = 1
-        
-    def update(self):
-        '''Updates counter. Depending on counter:
-            - does nothing
-            - changes image
-            - end animation by terminating sprite object'''
-            
-        self.counter += 1
-        
-        # Check if image needs to be updated / animation has ended
-        if self.counter % int(self.frames_per_image) == 0:
-            # if end of animation is NOT reached, continue animation
-            if self.image_index < self.last_image_index:
-                self.image_index += 1 # update image index
-                self.image = self._original_images[self.image_index] # update animaion surface
-            # if end of animation IS reached terminate animation sprite
-            elif self.image_index == self.last_image_index:
-                self.kill() # cleanly remove animation sprite
-        
-
-def main():
-    '''Runs an animation demo of above class.'''
-
-    # intialize pygame
-    pg.init()
-        
-    # create clock    
-    clock = pg.time.Clock()
-    
-    white = 255, 255, 255
-    fps = 60 # frames per second for pygame
-    spi = 0.08 # seconds per image for animation
-    #frames_between_animation = 500 # pause between enemy death and spawning of new enemy in seconds
-    
-    # initialize main screen
-    size = width, height = 1040, 740 # screen size
-    screen = pg.display.set_mode(size)
-    
-    # initialize empty animation group
-    animation_group = Group()
-    
-    # create animation
-    BasicAnimation(520,
-                   370,
-                   spi,
-                   fps,
-                   animation_group)
-    
-    # start main game loop
-    while True:
-        # check for exit events
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-        
-        # update sprites
-        animation_group.update()
-        
-        # draw new game state    
-        screen.fill(white) # paint over old game state
-        
-        animation_group.draw(screen) # draw all player sprite
-                   
-        # flip canvas
-        pg.display.flip()       
-            
-        # control framerate
-        clock.tick(fps)                
-
-if __name__ == '__main__':
-    main()
