cceb80a9fd7ecf8a45c9a35ae2cacdaec10f3a71
bettmensch88 <sebastian.scherer@yahoo.de>
2020-04-11 16:51:12 +0100

    reset the working dir step so that exe build works

--- lib/star_wars_dogfighter.py
+++ lib/star_wars_dogfighter.py
@@ -30,7 +30,7 @@ class Game(object):
         '''Initializes the game object and also the game'''
         
         # chane into executable file directory
-        #os.chdir(os.path.join(os.getcwd(),'exe.win-amd64-3.6'))
+        os.chdir(os.path.join(os.getcwd(),'exe.win-amd64-3.6'))
         
         # initialize pygame (handles pretty much eveything)
         pg.init()
